e1b5f3d5927238c1323419b2dbfb9173c154fe50
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-06-01 14:25:37 +0100

    new,create and index set up

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,27 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  #what are we ceating what key/values need to be inputted.
+  private
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
